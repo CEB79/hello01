76c5f9d1f6268c194df8a8c7b2bc150b62b2d667
sun <sonhokg@naver.com>
2024-01-18 16:50:16 +0900

    0118_sun

--- vue/products/client/src/views/RecipeBoard.vue
+++ vue/products/client/src/views/RecipeBoard.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="RecipeBoard">
+    <button class="btn_add_add" id="write"><a class="write_a" href="RecipeRegister">글작성</a></button>
     <table class="Board_table">
       <tbody class="Board_tbody">
       <tr class="Board_tr">
@@ -36,72 +37,47 @@
     <!-- 썸네일 사진 넣기 -->
     <div class="RecipeThumbnail">
       <div class="row" id="thumbnail">
-
-        <div class='col-lg-4 col-md-6 col-sm-6'>
+        <div class='col-lg-4 col-md-6 col-sm-6' v-for="item in items" :key="item.RecipeID">
           <div class='thumbnail_item'>
             <a href="RecipeDetails">
-              <img class="thumbnail_item__pic" src="../../public/common/img/EB/7-1.jpg">
+              <img class="thumbnail_item__pic" :src="getImageUrl(item.TitleImg)" alt="">
               <div class='thumbnail_item__name'>
-                <h6>글제목</h6>
-                <div class='thumbnail_item__price'>글쓴이</div>
+                <h6>{{ item.RecipeName }}</h6>
+                <div class='thumbnail_item__price'>{{ item.userNa }}</div>
               </div>
             </a>
           </div>
         </div>
-
-        <div class='col-lg-4 col-md-6 col-sm-6'>
-          <div class='thumbnail_item'>
-            <img class="thumbnail_item__pic" src="../../public/common/img/EB/7-1.jpg">
-            <div class='thumbnail_item__name'>
-              <h6>글제목</h6>
-              <div class='thumbnail_item__price'>글쓴이</div>
-            </div>
-          </div>
-        </div>
-
-        <div class='col-lg-4 col-md-6 col-sm-6'>
-          <div class='thumbnail_item'>
-            <img class="thumbnail_item__pic" src="../../public/common/img/EB/7-1.jpg">
-            <div class='thumbnail_item__name'>
-              <h6>글제목</h6>
-              <div class='thumbnail_item__price'>글쓴이</div>
-            </div>
-          </div>
-        </div>
-
-        <div class='col-lg-4 col-md-6 col-sm-6'>
-          <div class='thumbnail_item'>
-            <img class="thumbnail_item__pic" src="../../public/common/img/EB/7-1.jpg">
-            <div class='thumbnail_item__name'>
-              <h6>글제목</h6>
-              <div class='thumbnail_item__price'>글쓴이</div>
-            </div>
-          </div>
-        </div>
-
-        <div class='col-lg-4 col-md-6 col-sm-6'>
-          <div class='thumbnail_item'>
-            <img class="thumbnail_item__pic" src="../../public/common/img/EB/7-1.jpg">
-            <div class='thumbnail_item__name'>
-              <h6>글제목</h6>
-              <div class='thumbnail_item__price'>글쓴이</div>
-            </div>
-          </div>
-        </div>
-
-        <div class='col-lg-4 col-md-6 col-sm-6'>
-          <div class='thumbnail_item'>
-            <img class="thumbnail_item__pic" src="../../public/common/img/EB/7-1.jpg">
-            <div class='thumbnail_item__name'>
-              <h6>글제목</h6>
-              <div class='thumbnail_item__price'>글쓴이</div>
-            </div>
-          </div>
-        </div>
-
       </div>
     </div>
   </section>
 </template>
-<script setup>
+<script>
+import axios from "axios";
+
+export default {
+  name: "RecipeBoard",
+  data() {
+    return {
+      items: [],
+    };
+  },
+  created() {
+    this.showRecipe();
+  },
+  methods: {
+    async showRecipe() {
+      try {
+        const response = await axios.get("http://localhost:5000/recipe");
+        this.items = response.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    // 문자열 형태의 이미지 주소를 이미지 URL로 변환하는 메서드
+    getImageUrl(titleImg) {
+      return `${titleImg}`;
+    },
+  }
+}
 </script>
