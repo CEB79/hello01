d23479d0ec043a56d072223e6b26a1c50c7d493c
CEB79 <dmsqo79@gmail.com>
2024-01-19 16:33:32 +0900

    01.19 4

--- vue/products/backend/routes/routes.js
+++ vue/products/backend/routes/routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 //EB
-import {checkLogin, checkUserById, createUser, deleteUser, showUser} from "./controllers/User.js";
+import {checkLogin, checkUserById, createUser, deleteUser, getIdtoUser, getPwtoUser, showUser} from "./controllers/User.js";
 //SUN
 import {showRecipe, showRecipeDetails, showRecipeIMG} from "./controllers/RBoard.js";
 
@@ -11,11 +11,9 @@ const router = express.Router();
 
 //EB
 router.get('/user', showUser);
-
-// Get Single User
+router.post('/userId', getIdtoUser);
+router.post('/userPw', getPwtoUser);
 router.post('/user/:id', checkUserById);
-
-// router.post('/user', createUser);
 router.post('/user', createUser);
 router.delete('/user/:id', deleteUser);
 router.post('/login', checkLogin);
