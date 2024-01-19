8742709212d9aa8cfcd613788b5ca778dc2eeca9
youjiyou82 <youjiyou82@gmail.com>
2024-01-19 13:50:14 +0900

    01-19-03-JY

--- vue/products/backend/routes/controllers/Product_JY.js
+++ vue/products/backend/routes/controllers/Product_JY.js
@@ -1,28 +1,28 @@
-import { mypageInfo} from "../Models/ProductModel_JY.js"
-
+import db from "../../config/database.js";
 
 //mypage 회원번호, 이름, 전화번호 부분//
-export const showMypage = (req, res) => {
-    let UserNo = req.body.UserNo;
-    mypageInfo(UserNo,(err, results) => {
+export const mypageInfo = (UserNo, result) => {
+    // console.log(UserNo);
+    // db.query("SELECT User.UserNo, user.UserNa, user.UserPhon FROM mypage INNER JOIN user ON mypage.FK__user=user.UserNo WHERE mypage.FK__user=?", [UserNo], (err, results) => {
+    db.query("SELECT user.UserNo, user.UserNa, user.UserPhon, COUNT(MYPAGE.FK__fboard), COUNT(MYPAGE.FK__rboard) FROM user LEFT JOIN MYPAGE ON user.UserNo = MYPAGE.FK__user WHERE user.UserNo=2", [UserNo], (err, results) => {
         if(err) {
-            res.send(err);
+            console.log(err);
+            result(err, null);
         } else {
-            console.log(results);
-            res.json(results);
+            result(null, results);
         }
     });
 }
 
 //mypage fboard 자유게시판 글 쓴 개수//
-// export const showMypage_fboard_count = (req, res) => {
-//     let UserNo = req.body.UserNo;
-//     mypageInfo_fboard_count(UserNo,(err, results) => {             
+// export const mypageInfo_fboard_count = (UserNo, result) => {
+//     // console.log(UserNo);
+//     db.query("SELECT COUNT(mypage.FK__fboard) FROM mypage WHERE mypage.FK__user=?", [UserNo], (err, results) => {
 //         if(err) {
-//             res.send(err);
+//             console.log(err);
+//             result(err, null);
 //         } else {
-//             console.log(results);
-//             res.json(results);
+//             result(null, results);
 //         }
 //     });
 // }
